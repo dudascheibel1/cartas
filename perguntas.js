70342c28d15f634f463742822132503be15e7484
dudascheibel1 <dudascheibel7@gmail.com>
2024-11-27 16:07:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Série',
+    'Quem é a identidade secreta da Ladybug?',
+    'A identidade secreta da Ladybug é Marinette Dupain-Cheng.'
+)
+
+criaCartao(
+    'filme',
+    'Qual é o nome do vampiro que se apaixona por Bella Swan na saga Crepúsculo?',
+    'O nome do vampiro é Edward Cullen. Ele se apaixona por Bella Swan, uma humana, e juntos enfrentam vários desafios ao longo da história.'
+)
+
+criaCartao(
+    'Série',
+    ' Quem é o parceiro de Ladybug e qual é a sua identidade secreta?',
+    'O parceiro de Ladybug é o Cat Noir (ou Chat Noir), e sua identidade secreta é Adrien Agreste.'
+)
+
+criaCartao(
+    'Dorama',
+    'Qual é o nome da protagonista do dorama Beleza Verdadeira e qual é o seu segredo?',
+    ' A protagonista do dorama Beleza Verdadeira se chama Lim Ju-kyung. Seu segredo é que, apesar de ser considerada uma "beleza" após aprender a se maquiar, ela na verdade tem uma aparência natural que ela considera comum e insegura. Ao longo da história, ela tenta esconder seu rosto sem maquiagem, temendo ser rejeitada pelas pessoas ao seu redor.'
+)
